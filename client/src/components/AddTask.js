9336df40424a05edffed4474d61c51e9692757ce
Dchole <derekoware47@gmail.com>
2020-02-22 19:30:33 +0000

    Confirm dialog

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Fab } from "@material-ui/core"
+import Fab from "@material-ui/core/Fab"
 import AddIcon from "@material-ui/icons/Add"
 
 const AddTask = () => (
--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { taskContext } from "../context/taskContext"
 import Typography from "@material-ui/core/Typography"
 import IconButton from "@material-ui/core/IconButton"
@@ -15,9 +15,13 @@ import {
   ListItem,
   Switch
 } from "@material-ui/core"
+import ConfirmDelete from "./ConfirmDelete"
+
+let task_id
 
 const TodoList = () => {
-  const { tasks, handleDelete, handleCompleted } = useContext(taskContext)
+  const { tasks, handleCompleted } = useContext(taskContext)
+  const [open, setOpen] = useState(false)
 
   const completedStyles = {
     fontStyle: "italic",
@@ -25,79 +29,91 @@ const TodoList = () => {
     opacity: 0.5
   }
 
+  const handleOpen = () => setOpen(true)
+
+  const handleClose = () => setOpen(false)
+
   return (
-    <List>
-      {tasks.map(task => (
-        <ListItem key={task._id}>
-          <ExpansionPanel style={{ width: "100%" }}>
-            <ExpansionPanelSummary>
-              <Typography
-                variant="h6"
-                style={
-                  task.status
-                    ? {
-                        ...completedStyles,
-                        flexGrow: 1,
-                        paddingTop: 7,
-                        textTransform: "capitalize"
-                      }
-                    : {
-                        flexGrow: 1,
-                        paddingTop: 7,
-                        textTransform: "capitalize"
-                      }
-                }
-              >
-                {task.title}
-              </Typography>
-              <FormControlLabel
-                aria-label="Task Status"
-                onChange={_ => handleCompleted(task._id)}
-                control={
-                  <Switch
-                    color="primary"
-                    checked={task.status}
-                    onChange={_ => handleCompleted(task._id)}
-                  />
-                }
-              />
-              <IconButton onClick={_ => handleDelete(task._id)}>
-                <DeleteIcon color="secondary" />
-              </IconButton>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography
-                variant="subtitle2"
-                color="textSecondary"
-                align="center"
-                component="p"
-                style={{ width: "100%" }}
-              >
-                {task.description}
-              </Typography>
-            </ExpansionPanelDetails>
-            <ExpansionPanelActions>
-              <Typography
-                variant="caption"
-                color={task.status ? "textSecondary" : "primary"}
-                component="small"
-                style={{ marginLeft: 20, flexGrow: 1, fontWeight: "bolder" }}
-              >
-                {new Date(task.expTime).toDateString()}
-              </Typography>
-              <Fab
-                size="small"
-                color="primary"
-                style={{ margin: 15 }}
-                disabled={task.status}
-              >
-                <EditIcon fontSize="small" />
-              </Fab>
-            </ExpansionPanelActions>
-          </ExpansionPanel>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <List>
+        {tasks.map(task => (
+          <ListItem key={task._id}>
+            <ExpansionPanel style={{ width: "100%" }}>
+              <ExpansionPanelSummary>
+                <Typography
+                  variant="h6"
+                  style={
+                    task.status
+                      ? {
+                          ...completedStyles,
+                          flexGrow: 1,
+                          paddingTop: 7,
+                          textTransform: "capitalize"
+                        }
+                      : {
+                          flexGrow: 1,
+                          paddingTop: 7,
+                          textTransform: "capitalize"
+                        }
+                  }
+                >
+                  {task.title}
+                </Typography>
+                <FormControlLabel
+                  aria-label="Task Status"
+                  onChange={_ => handleCompleted(task._id)}
+                  control={
+                    <Switch
+                      color="primary"
+                      checked={task.status}
+                      onChange={_ => handleCompleted(task._id)}
+                    />
+                  }
+                />
+                <IconButton
+                  onClick={_ => {
+                    task_id = task._id
+                    handleOpen()
+                  }}
+                >
+                  <DeleteIcon color="secondary" />
+                </IconButton>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography
+                  variant="subtitle2"
+                  color="textSecondary"
+                  align="center"
+                  component="p"
+                  style={{ width: "100%" }}
+                >
+                  {task.description}
+                </Typography>
+              </ExpansionPanelDetails>
+              <ExpansionPanelActions>
+                <Typography
+                  variant="caption"
+                  color={task.status ? "textSecondary" : "primary"}
+                  component="small"
+                  style={{ marginLeft: 20, flexGrow: 1, fontWeight: "bolder" }}
+                >
+                  {new Date(task.expTime).toDateString()}
+                </Typography>
+                <Fab
+                  size="small"
+                  color="primary"
+                  style={{ margin: 15 }}
+                  disabled={task.status}
+                >
+                  <EditIcon fontSize="small" />
+                </Fab>
+              </ExpansionPanelActions>
+            </ExpansionPanel>
+          </ListItem>
+        ))}
+      </List>
+      <ConfirmDelete open={open} handleClose={handleClose} task_id={task_id} />
+    </>
   )
 }
 
