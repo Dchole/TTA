ac55000e85bc9d82bcca5953df2683d62ee1d493
Dchole <derekoware47@gmail.com>
2020-02-28 12:30:09 +0000

    Application Core Completed

--- client/src/components/Feedback.js
+++ client/src/components/Feedback.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react"
+import React, { useContext } from "react"
 import { Snackbar } from "@material-ui/core"
 import Alert from "@material-ui/lab/Alert"
 import { taskContext } from "../context/taskContext"
